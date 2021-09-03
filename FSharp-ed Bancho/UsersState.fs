2047f923737527d6aaee24b3ca4a8bdf1a3c397c
Amber Kowalski <amberkowalski03@gmail.com>
2021-09-03 09:50:53 -0500

    Delete useless file

--- FSharp-ed Bancho/UsersState.fs
+++
@@ -1,9 +0,0 @@
-﻿module FSharp_ed_Bancho.UsersState
-
-open System.Threading
-type UsersState() =
-    static let mutable state: int = 0
-
-    static member Increment =
-        state <- state + 1
-        $"%d{state}"
